5e59f239934ba46be45daf5741cdc43c6ba36bd3
romain.vanneste.etu <romain.vanneste.etu@univ-lille.fr>
2020-11-08 12:09:06 +0100

    feat: ajout des classes Player et Game avec le calcul de la probabilité qu'un joueur a gagne contre un joueur b et l'inverse

---
+++ model/Game.php
@@ -0,0 +1,63 @@
+<?php
+
+
+class Game
+{
+    public $player1;
+    public $player2;
+
+    function __construct($player1,$player2)
+    {
+        $this->player1=$player1;
+        $this->player2=$player2;
+    }
+
+    function probaPlayer1()
+    {
+        $exposant = ($this->player2-$this->player1)/400;
+        $division = 1+(10**$exposant);
+        return 1/$division;
+
+    }
+
+    function probaPlayer2()
+    {
+        $exposant = ($this->player1-$this->player2)/400;
+        $division = 1+(10**$exposant);
+        return 1/$division;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getPlayer1()
+    {
+        return $this->player1;
+    }
+
+    /**
+     * @param Player $player1
+     */
+    public function setPlayer1($player1)
+    {
+        $this->player1 = $player1;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getPlayer2()
+    {
+        return $this->player2;
+    }
+
+    /**
+     * @param Player $player2
+     */
+    public function setPlayer2($player2)
+    {
+        $this->player2 = $player2;
+    }
+
+
+}
---
+++ model/Player.php
@@ -0,0 +1,47 @@
+<?php
+
+class Player{
+    public $name;
+    public $level;
+
+    function __construct($name)
+    {
+        $this->name=$name;
+        $this->level=1200;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * @param int $level
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+    }
+
+
+}
+?>
